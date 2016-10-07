8dc1e278cb193facbf1ef25d22c7a27c4c3e6a6a
Justin Igawa <J_Igawa@justins-air.attlocal.net>
2016-10-07 11:07:11 -0700

    copied da_burger

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Chili Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hawaiian Burger');
+INSERT INTO burgers (burger_name) VALUES ('Salmon Burger');
